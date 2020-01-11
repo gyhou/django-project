7cab17d6012359388c4249929debb34594bf38ba
George Hou <George Hou>
2020-01-11 14:06:45 -0800

    added navbar and header

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tutorial
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("<strong>Django</strong> project")
+    # return HttpResponse("<strong>Django</strong> project")
+    return render(request=request,
+                  template_name='main/home.html',
+                  context={'tutorials':Tutorial.objects.all})
