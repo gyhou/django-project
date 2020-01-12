c58a34ae2d7a44fda1a19fc30eacdcad03312bd7
George Hou <George Hou>
2020-01-11 22:28:15 -0800

    create login/logout

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
@@ -16,14 +17,42 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
             login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}:{form.error_messages[msg]}")
 
     form = UserCreationForm
     return render(request,
                   "main/register.html",
                   context={'form':form})
-                  
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    form = AuthenticationForm()
+    return render(request,
+                  "main/login.html",
+                  {'form':form})
