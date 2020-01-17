3e42e0d84a625bc9c6afb7d39aeeea476fad3050
George Hou <George Hou>
2020-01-16 19:58:51 -0800

    update settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -107,14 +107,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-DATABASES = {'default': dj_database_url.config(
-    default=config('DATABASE_URL'), conn_max_age=600)}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+# DATABASES = {'default': dj_database_url.config(
+#     default=config('DATABASE_URL'), conn_max_age=600)}
 
 
 # Password validation
